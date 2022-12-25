7eeb97c16f9fa609d747cdc03edafedc3458db45
Duanshuiliu2002 <114049196+Duanshuiliu2002@users.noreply.github.com>
2022-12-25 15:58:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "老婆",
       // 微信测试号字符串
-      id: "omHE5617RCQBY4BkWza1O1ab9Jc4",
+      id: "omHE56xq-q_YQWrQuHabu0z9wiJ0",
       // 模板消息ID
       useTemplateId: "K1ZrkPFOirlrQ8pLLswN5O7MJBf0ntq1SZHzxRZKdf8",
       province: "江苏",
